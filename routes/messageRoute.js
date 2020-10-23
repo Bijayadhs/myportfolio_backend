8673979d3842dd0cfe89fe4bdb8e9580e0cc6008
Bijayadhs <bijayadhikari107@gmail.com>
2020-10-23 16:29:23 +1100

    mongodb connected

--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -1,13 +1,21 @@
 import express from 'express';
+import messageModel from '../../models/messageModel.js';
 const router = express.Router();
+
 
 router.get('/detail', (req, res) => {
     try {
-        res.status(200).json({ message: 'All my details' })
+        res.send('All my details')
     } catch (err) {
         console.log(err);
-        res.json({ error: err })
+        res.send({ error: err })
     }
+})
+
+router.post('/message', (req, res) => {
+    const { name, email, phone, message } = req.body;
+    const msg = new messageModel({ name, email, phone, message }).save();
+    res.json({ msg })
 })
 
 
